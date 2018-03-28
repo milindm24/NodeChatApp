bc17b38254e1f23c23663272334dc26299a35c34
LAPTOP-2MJRKRGJ\Milind <milindm24@gmail.com>
2018-03-27 23:47:23 -0500

    style

--- public/js/index.js
+++ public/js/index.js
@@ -29,15 +29,17 @@ socket.on('newMessage', function(message) {
 //     console.log('Got it', data);
 // });
 
+var messageTextbox = jQuery('[name=message]');
+
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
 
     socket.emit('createMessage',{
         from:'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-
+        messageTextbox.val('');
     });
 });
 
@@ -49,13 +51,19 @@ locationButton.on('click', function(){
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+
+        locationButton.removeAttr('disabled').text('Send Location');
     
     }, function(){
+        locationButton.removeAttr('disabled').text('Send Location');        
         alert('Unable to fetch location');
     })
 });
